c4d84e6db68419daad359e280368b9c3771f7ba6
edurso <eric@casadurso.com>
2021-06-02 10:49:25 -0400

    PDEV-108 clean up characterization project

--- characterization/src/main/java/dc/Robot.java
+++ characterization/src/main/java/dc/Robot.java
@@ -55,246 +55,209 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import java.util.ArrayList; 
+import java.util.ArrayList;
 
 public class Robot extends TimedRobot {
 
-  // static private double ENCODER_EDGES_PER_REV = 8192;
-  // static private int PIDIDX = 0;
-  // static private int ENCODER_EPR = 8192;
-  // static private double GEARING = 1;
-  
-  // private double encoderConstant = (1 / GEARING) * (1 / ENCODER_EDGES_PER_REV);
+	Joystick stick;
+	DifferentialDrive drive;
 
-  Joystick stick;
-  DifferentialDrive drive;
+	Supplier<Double> leftEncoderPosition;
+	Supplier<Double> leftEncoderRate;
+	Supplier<Double> rightEncoderPosition;
+	Supplier<Double> rightEncoderRate;
+	Supplier<Double> gyroAngleRadians;
 
+	NetworkTableEntry autoSpeedEntry = NetworkTableInstance.getDefault().getEntry("/robot/autospeed");
+	NetworkTableEntry telemetryEntry = NetworkTableInstance.getDefault().getEntry("/robot/telemetry");
+	NetworkTableEntry rotateEntry = NetworkTableInstance.getDefault().getEntry("/robot/rotate");
 
-  Supplier<Double> leftEncoderPosition;
-  Supplier<Double> leftEncoderRate;
-  Supplier<Double> rightEncoderPosition;
-  Supplier<Double> rightEncoderRate;
-  Supplier<Double> gyroAngleRadians;
+	String data = "";
 
-  NetworkTableEntry autoSpeedEntry = NetworkTableInstance.getDefault().getEntry("/robot/autospeed");
-  NetworkTableEntry telemetryEntry = NetworkTableInstance.getDefault().getEntry("/robot/telemetry");
-  NetworkTableEntry rotateEntry = NetworkTableInstance.getDefault().getEntry("/robot/rotate");
+	int counter = 0;
+	double startTime = 0;
+	double priorAutospeed = 0;
 
-  String data = "";
-  
-  int counter = 0;
-  double startTime = 0;
-  double priorAutospeed = 0;
+	double[] numberArray = new double[10];
+	ArrayList<Double> entries = new ArrayList<Double>();
 
-  double[] numberArray = new double[10];
-  ArrayList<Double> entries = new ArrayList<Double>();
-  public Robot() {
-    super(.005);
-    LiveWindow.disableAllTelemetry();
-  }
+	public Robot() {
+		super(.005);
+		LiveWindow.disableAllTelemetry();
+	}
 
-  public enum Sides {
-    LEFT,
-    RIGHT,
-    FOLLOWER
-  }
+	public enum Sides {
+		LEFT, RIGHT, FOLLOWER
+	}
 
-  // methods to create and setup motors (reduce redundancy)
-  public WPI_TalonFX setupWPI_TalonFX(int port, Sides side, boolean inverted) {
-    // create new motor and set neutral modes (if needed)
-    WPI_TalonFX motor = new WPI_TalonFX(port);
-    motor.setInverted(inverted);
-    
-    // setup encoder if motor isn't a follower
-    if (side != Sides.FOLLOWER) {
-    
-      // CANCoder frontEncoder;
-      // CANCoder backEncoder;
+	// methods to create and setup motors (reduce redundancy)
+	public WPI_TalonFX setupWPI_TalonFX(int port, Sides side, boolean inverted) {
+		// create new motor and set neutral modes (if needed)
+		WPI_TalonFX motor = new WPI_TalonFX(port);
+		motor.setInverted(inverted);
 
+		// setup encoder if motor isn't a follower
+		if (side != Sides.FOLLOWER) {
 
+			switch (side) {
+				// setup encoder and data collecting methods
 
-    switch (side) {
-      // setup encoder and data collecting methods
+				case RIGHT:
+					rightEncoderPosition = () -> ((double) motor.getSelectedSensorPosition() / (2048.0 * 6.86)) * (1 / 3.0 * Math.PI);
+					rightEncoderRate = () -> (((double) motor.getSelectedSensorVelocity() * 10) / (2048.0 * 6.86)) * (1 / 3.0 * Math.PI);
+					break;
+				case LEFT:
+					leftEncoderPosition = () -> ((double) motor.getSelectedSensorPosition() / (2048.0 * 6.86)) * (1 / 3.0 * Math.PI);
+					leftEncoderRate = () -> (((double) motor.getSelectedSensorVelocity() * 10) / (2048.0 * 6.86)) * (1 / 3.0 * Math.PI);
+					break;
+				default:
+					// probably do nothing
+					break;
 
-      case RIGHT:
-        // set right side methods = encoder methods
+			}
 
-        motor.getSelectedSensorPosition();
-        // frontEncoder = new CANCoder(17);
-        // frontEncoder.configSensorDirection(false);
-        // backEncoder = new CANCoder%(18);
-        // backEncoder.configSensorDirection(false);
+		}
 
-        // frontEncoder. setDistancePerPulse((double) encoderConstant / 4);
-        rightEncoderPosition = () -> ((double) motor.getSelectedSensorPosition() / (2048.0 * 6.86)) * (1/3.0 * Math.PI) ;
-        rightEncoderRate = () -> (((double) motor.getSelectedSensorVelocity() * 10) / (2048.0 * 6.86)) * (1/3.0 * Math.PI);
+		return motor;
 
-        break;
-      case LEFT:
-        // frontEncoder = new CANCoder(16);
-        // frontEncoder.configSensorDirection(false);
-        // backEncoder = new CANCoder(15);
-        // backEncoder.configSensorDirection(false);
+	}
 
-        // encoder.setDistancePerPulse((double) encoderConstant / 4);
-        leftEncoderPosition = () -> ((double) motor.getSelectedSensorPosition() / (2048.0 * 6.86)) * (1/3.0 * Math.PI);
-        leftEncoderRate = () -> (((double) motor.getSelectedSensorVelocity() * 10) / (2048.0 * 6.86)) * (1/3.0 * Math.PI);
+	@Override
+	public void robotInit() {
+		if (!isReal()) SmartDashboard.putData(new SimEnabler());
 
+		stick = new Joystick(0);
 
-        break;
-      default:
-        // probably do nothing
-        break;
+		// create left motor
+		WPI_TalonFX leftMotor = setupWPI_TalonFX(8, Sides.LEFT, false);
+		ArrayList<SpeedController> leftMotors = new ArrayList<SpeedController>();
+		leftMotors.add(setupWPI_TalonFX(10, Sides.FOLLOWER, false));
+		SpeedController[] leftMotorControllers = new SpeedController[leftMotors.size()];
+		leftMotorControllers = leftMotors.toArray(leftMotorControllers);
+		SpeedControllerGroup leftGroup = new SpeedControllerGroup(leftMotor, leftMotorControllers);
 
-      }
-    
-    }
-    
+		WPI_TalonFX rightMotor = setupWPI_TalonFX(11, Sides.RIGHT, false);
+		ArrayList<SpeedController> rightMotors = new ArrayList<SpeedController>();
+		rightMotors.add(setupWPI_TalonFX(13, Sides.FOLLOWER, false));
+		SpeedController[] rightMotorControllers = new SpeedController[rightMotors.size()];
+		rightMotorControllers = rightMotors.toArray(rightMotorControllers);
+		SpeedControllerGroup rightGroup = new SpeedControllerGroup(rightMotor, rightMotorControllers);
+		drive = new DifferentialDrive(leftGroup, rightGroup);
+		drive.setDeadband(0);
 
-    return motor;
+		//
+		// Configure gyro
+		//
 
-  }
+		// Note that the angle from the NavX and all implementors of WPILib Gyro
+		// must be negated because getAngle returns a clockwise positive angle
+		// Uncomment for Pigeon
+		PigeonIMU pigeon = new PigeonIMU(19);
+		gyroAngleRadians = () -> {
+			// Allocating a new array every loop is bad but concise
+			double[] xyz = new double[3];
+			pigeon.getAccumGyro(xyz);
+			return Math.toRadians(xyz[2]);
+		};
 
-  @Override
-  public void robotInit() {
-    if (!isReal()) SmartDashboard.putData(new SimEnabler());
+		// Set the update rate instead of using flush because of a ntcore bug
+		// -> probably don't want to do this on a robot in competition
+		NetworkTableInstance.getDefault().setUpdateRate(0.010);
+	}
 
-    stick = new Joystick(0);
-    
-    // create left motor
-    WPI_TalonFX leftMotor = setupWPI_TalonFX(8, Sides.LEFT, false);
+	@Override
+	public void disabledInit() {
+		double elapsedTime = Timer.getFPGATimestamp() - startTime;
+		System.out.println("Robot disabled");
+		drive.tankDrive(0, 0);
+		// data processing step
+		data = entries.toString();
+		data = data.substring(1, data.length() - 1) + ", ";
+		telemetryEntry.setString(data);
+		entries.clear();
+		System.out.println("Robot disabled");
+		System.out.println("Collected : " + counter + " in " + elapsedTime + " seconds");
+		data = "";
+	}
 
-    ArrayList<SpeedController> leftMotors = new ArrayList<SpeedController>();
-    leftMotors.add(setupWPI_TalonFX(10, Sides.FOLLOWER, false));
-    SpeedController[] leftMotorControllers = new SpeedController[leftMotors.size()];
-    leftMotorControllers = leftMotors.toArray(leftMotorControllers);
-    SpeedControllerGroup leftGroup = new SpeedControllerGroup(leftMotor, leftMotorControllers);
+	@Override
+	public void disabledPeriodic() {
+	}
 
-    WPI_TalonFX rightMotor = setupWPI_TalonFX(11, Sides.RIGHT, false);
-    ArrayList<SpeedController> rightMotors = new ArrayList<SpeedController>();
-    rightMotors.add(setupWPI_TalonFX(13, Sides.FOLLOWER, false));
-    SpeedController[] rightMotorControllers = new SpeedController[rightMotors.size()];
-    rightMotorControllers = rightMotors.toArray(rightMotorControllers);
-    SpeedControllerGroup rightGroup = new SpeedControllerGroup(rightMotor, rightMotorControllers);
-    drive = new DifferentialDrive(leftGroup, rightGroup);
-    drive.setDeadband(0);
+	@Override
+	public void robotPeriodic() {
+		// feedback for users, but not used by the control program
+		SmartDashboard.putNumber("l_encoder_pos", leftEncoderPosition.get());
+		SmartDashboard.putNumber("l_encoder_rate", leftEncoderRate.get());
+		SmartDashboard.putNumber("r_encoder_pos", rightEncoderPosition.get());
+		SmartDashboard.putNumber("r_encoder_rate", rightEncoderRate.get());
+	}
 
-    //
-    // Configure gyro
-    //
+	@Override
+	public void teleopInit() {
+		System.out.println("Robot in operator control mode");
+	}
 
-    // Note that the angle from the NavX and all implementors of WPILib Gyro
-    // must be negated because getAngle returns a clockwise positive angle
-    // Uncomment for Pigeon
-    PigeonIMU pigeon = new PigeonIMU(19);
-    gyroAngleRadians = () -> {
-      // Allocating a new array every loop is bad but concise
-      double[] xyz = new double[3];
-      pigeon.getAccumGyro(xyz);
-      return Math.toRadians(xyz[2]);
-    };
+	@Override
+	public void teleopPeriodic() {
+		drive.arcadeDrive(-stick.getY(), stick.getX());
+	}
 
-    // Set the update rate instead of using flush because of a ntcore bug
-    // -> probably don't want to do this on a robot in competition
-    NetworkTableInstance.getDefault().setUpdateRate(0.010);
-  }
+	@Override
+	public void autonomousInit() {
+		System.out.println("Robot in autonomous mode");
+		startTime = Timer.getFPGATimestamp();
+		counter = 0;
+	}
 
-  @Override
-  public void disabledInit() {
-    double elapsedTime = Timer.getFPGATimestamp() - startTime;
-    System.out.println("Robot disabled");
-    drive.tankDrive(0, 0);
-    // data processing step
-    data = entries.toString();
-    data = data.substring(1, data.length() - 1) + ", ";
-    telemetryEntry.setString(data);
-    entries.clear();
-    System.out.println("Robot disabled");
-    System.out.println("Collected : " + counter + " in " + elapsedTime + " seconds");
-    data = "";
-  }
+	/**
+	 * If you wish to just use your own robot program to use with the data logging
+	 * program, you only need to copy/paste the logic below into your code and
+	 * ensure it gets called periodically in autonomous mode
+	 * 
+	 * Additionally, you need to set NetworkTables update rate to 10ms using the
+	 * setUpdateRate call.
+	 */
+	@Override
+	public void autonomousPeriodic() {
 
-  @Override
-  public void disabledPeriodic() {
-  }
+		// Retrieve values to send back before telling the motors to do something
+		double now = Timer.getFPGATimestamp();
 
-  @Override
-  public void robotPeriodic() {
-    // feedback for users, but not used by the control program
-    SmartDashboard.putNumber("l_encoder_pos", leftEncoderPosition.get());
-    SmartDashboard.putNumber("l_encoder_rate", leftEncoderRate.get());
-    SmartDashboard.putNumber("r_encoder_pos", rightEncoderPosition.get());
-    SmartDashboard.putNumber("r_encoder_rate", rightEncoderRate.get());
-  }
+		double leftPosition = leftEncoderPosition.get();
+		double leftRate = leftEncoderRate.get();
 
-  @Override
-  public void teleopInit() {
-    System.out.println("Robot in operator control mode");
-  }
+		double rightPosition = rightEncoderPosition.get();
+		double rightRate = rightEncoderRate.get();
 
-  @Override
-  public void teleopPeriodic() {
-    drive.arcadeDrive(-stick.getY(), stick.getX());
-  }
+		double battery = RobotController.getBatteryVoltage();
+		double motorVolts = battery * Math.abs(priorAutospeed);
 
-  @Override
-  public void autonomousInit() {
-    System.out.println("Robot in autonomous mode");
-    startTime = Timer.getFPGATimestamp();
-    counter = 0;
-  }
+		double leftMotorVolts = motorVolts;
+		double rightMotorVolts = motorVolts;
 
-  /**
-  * If you wish to just use your own robot program to use with the data logging
-  * program, you only need to copy/paste the logic below into your code and
-  * ensure it gets called periodically in autonomous mode
-  * 
-  * Additionally, you need to set NetworkTables update rate to 10ms using the
-  * setUpdateRate call.
-  */
-  @Override
-  public void autonomousPeriodic() {
+		// Retrieve the commanded speed from NetworkTables
+		double autospeed = autoSpeedEntry.getDouble(0);
+		priorAutospeed = autospeed;
 
-    // Retrieve values to send back before telling the motors to do something
-    double now = Timer.getFPGATimestamp();
+		// command motors to do things
+		drive.tankDrive((rotateEntry.getBoolean(false) ? -1 : 1) * autospeed, -autospeed, false);
 
-    double leftPosition = leftEncoderPosition.get();
-    double leftRate = leftEncoderRate.get();
+		numberArray[0] = now;
+		numberArray[1] = battery;
+		numberArray[2] = autospeed;
+		numberArray[3] = leftMotorVolts;
+		numberArray[4] = rightMotorVolts;
+		numberArray[5] = leftPosition;
+		numberArray[6] = rightPosition;
+		numberArray[7] = leftRate;
+		numberArray[8] = rightRate;
+		numberArray[9] = gyroAngleRadians.get();
 
-    double rightPosition = rightEncoderPosition.get();
-    double rightRate = rightEncoderRate.get();
-
-    double battery = RobotController.getBatteryVoltage();
-    double motorVolts = battery * Math.abs(priorAutospeed);
-
-    double leftMotorVolts = motorVolts;
-    double rightMotorVolts = motorVolts;
-
-    // Retrieve the commanded speed from NetworkTables
-    double autospeed = autoSpeedEntry.getDouble(0);
-    priorAutospeed = autospeed;
-
-    // command motors to do things
-    drive.tankDrive(
-      (rotateEntry.getBoolean(false) ? -1 : 1) * autospeed, -autospeed,
-      false
-    );
-
-    numberArray[0] = now;
-    numberArray[1] = battery;
-    numberArray[2] = autospeed;
-    numberArray[3] = leftMotorVolts;
-    numberArray[4] = rightMotorVolts;
-    numberArray[5] = leftPosition;
-    numberArray[6] = rightPosition;
-    numberArray[7] = leftRate;
-    numberArray[8] = rightRate;
-    numberArray[9] = gyroAngleRadians.get();
-
-    // Add data to a string that is uploaded to NT
-    for (double num : numberArray) {
-      entries.add(num);
-    }
-    counter++;
-  }
+		// Add data to a string that is uploaded to NT
+		for (double num : numberArray) {
+			entries.add(num);
+		}
+		counter++;
+	}
 }
